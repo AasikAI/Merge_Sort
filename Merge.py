c30525194ea4deea608f74607360a9bd24f6dead
AasikAI <aasikofficial03@gmail.com>
2023-03-23 12:31:05 +0530

    First Commit

---
+++ Merge.py
@@ -0,0 +1,36 @@
+def merge(arr):
+    if len(arr)>2:
+        mid = len(arr)//2
+        l = arr[:mid]
+        r = arr[mid:]
+        merge(l)
+        merge(r)
+        i = j = k = 0
+        while i<len(l) and j<len(r):
+            if l[i]<=r[j]:
+                arr[k] = l[i]
+                i+=1
+            else:
+                arr[k] = r[j]
+                j+=1
+            k+=1
+        while i<len(l):
+            arr[k] = l[i]
+            i+=1
+            k+=1
+        while j<len(r):
+            arr[k] = r[j]
+            j+=1
+            k+=1
+def printl(arr):
+    for i in range(len(arr)):
+        print(arr[i], end=" ")
+    print("\r")
+if __name__ == '__main__':
+    arr = [5,2,87,3,8,354,99,13,46,76,324,890,33,7867,89]
+    print("Unsorted array |")
+    printl(arr)
+    print("Sorted array |")
+    merge(arr)
+    printl(arr)
+
